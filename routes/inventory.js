b4199e12327158e76591c9f292db494e7cebe20a
Laura <laurafreda44@gmail.com>
2024-03-19 17:34:49 -0700

    routes added

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const inventoryController = require("../controllers/inventories-controller");
 
+router
+    .route("/")
+    .get(inventoryController.getInventories);
+
 router.route("/:id").put(inventoryController.updateInventory);
 
 module.exports = router;
