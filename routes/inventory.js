8021aca6682604f17b7b98924ef91aa393119757
Laura <laurafreda44@gmail.com>
2024-03-20 13:19:45 -0700

    Single inventory get request added

--- routes/inventory.js
+++ routes/inventory.js
@@ -6,6 +6,9 @@ router
     .get(inventoryController.getInventories)
     .post(inventoryController.addNewInventoryItem);
 
-router.route("/:id").put(inventoryController.updateInventory);
+router
+    .route("/:id")
+    .put(inventoryController.updateInventory)
+    .get(inventoryController.getSingleInventory);
 
 module.exports = router;
