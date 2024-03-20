0a1af3dacbcb445e1973548f4b19ec0b199c06a3
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-03-19 20:29:24 -0400

    adding add new inventory item method

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const inventoryController = require("../controllers/inventories-controller");
 
-router.route("/:id").put(inventoryController.updateInventory);
+router.route("/:id").put(inventoryController.updateInventory)
+.post(inventoryController.addNewInventoryItem);
 
 module.exports = router;
