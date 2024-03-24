00429003d38128a16e9b9218f9c9beb85cd2482e
Tonie Opara <146739904+tonynuns@users.noreply.github.com>
2024-03-23 18:50:37 -0600

    Update inventory.js
    Update getInventoriesByWarehouseId api

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const inventoryController = require("../controllers/inventories-controller");
 
-router.get("/:id/inventories", inventoryController.getInventoriesByWarehouseId);
-
 router
     .route("/")
     .get(inventoryController.getInventories)
